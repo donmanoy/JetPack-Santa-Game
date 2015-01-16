33f669a050742f5371a8cd12ace2dbc58c2a266a
Donn Dacalanio <donmanoy@yahoo.com>
2015-01-16 17:20:29 +0800

    initial commit
    creation

---
+++ src/game/config.js
@@ -0,0 +1,22 @@
+pandaConfig = {
+    name: 'Flying Dog',
+    version: '1.0.0',
+    
+    system: {
+        width: 768,
+        height: 1024,
+        rotateScreen: false,
+        bgColor: '#91f7ff'
+    },
+    analytics: {
+        id: 'UA-42024756-5'
+    },
+    storage: {
+        id: 'net.pandajs.flyingdog'
+    },
+    iOS8: {
+        system: {
+            webGL: true
+        }
+    }
+};
